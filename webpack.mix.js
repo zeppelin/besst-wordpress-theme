d0a247e01c515380361b4028722c01de6177510c
Gabor Babicz <gabor.babicz@gmail.com>
2019-11-20 22:44:55 +0100

    Enhance PostCSS config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,11 @@
 const mix = require('laravel-mix');
+require('laravel-mix-postcss-config');
 const tailwindcss = require('tailwindcss');
 
-mix.postCss('./theme-style.css', './style.css',
-  tailwindcss('./tailwind.config.js')
-);
+mix.postCss('./theme-style.css', './style.css').postCssConfig({
+  plugins: (loader) => [
+    require('postcss-import')({ root: loader.resourcePath }),
+    tailwindcss('./tailwind.config.js'),
+    require('postcss-nested')
+  ]
+});
